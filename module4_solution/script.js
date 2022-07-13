a9faec4fdd89b3f23121084cc95aa3b3ff5ac442
Payalsarde <sardepayal@gmail.com>
2022-07-13 11:33:39 +0530

    done

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+// (Note, Step 2 will be done in the SpeakHello.js file.)2
 (function () {
 
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
